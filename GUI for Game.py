ff2c75e32818905eb44288518fe4ac323c639215
GodlyAzn <zarc321@gmail.com>
2016-05-17 10:24:07 -0700

    GUI

---
+++ GUI for Game.py
@@ -0,0 +1,25 @@
+import Tkinter
+
+root = Tkinter.Tk()
+root.title("MAP")
+canvas = Tkinter.Canvas(root,height = 300, width = 600, relief = Tkinter.RAISED , bg = 'white')
+canvas.grid(row=0,column=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
